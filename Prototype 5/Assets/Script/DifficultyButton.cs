e58ff75085c3baa6d501c6011ac1a7881b0009ff
Seren Cem Tuğran <serentugran@gmail.com>
2022-09-04 18:05:06 +0300

    Prototype_5
    Test

---
+++ Prototype 5/Assets/Script/DifficultyButton.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButton : MonoBehaviour
+{
+    private Button button;
+    private GameManager gameManager;
+
+    public int difficulty;
+    void Start()
+    {
+        button = GetComponent<Button>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        button.onClick.AddListener(SetDifficulty);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void SetDifficulty()
+    {
+        Debug.Log(button.gameObject.name + " was clicked");
+        gameManager.StartGame(difficulty);
+    }
+}
